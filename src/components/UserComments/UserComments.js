de4449657a78978ac69e44fc331f3f93c64261ea
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2023-03-26 17:58:26 +0300

    .

--- src/components/UserComments/UserComments.js
+++ src/components/UserComments/UserComments.js
@@ -1,17 +1,31 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState, useEffect } from 'react';
 import { faUser } from '@fortawesome/fontawesome-free-regular';
+import { UseAuth } from '../../contexts/AuthContext';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../config/firebase';
 
 
 const UserComments = () => {
     const [commentText, setCommentText] = useState('');
+    const { user } = UseAuth();
+
+    const commentsCollectionRef = collection(db, 'comments');
+    
+    const getCommentsDocs = async () => {
+        const getComments = await getDocs(commentsCollectionRef)
+        getComments.forEach((doc) => {
+            console.log(doc.data());
+        })
+    }
+    getCommentsDocs();
+
+
+    const currentUserId = user.uid;
+    const currentUserEmail = user.email;
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
-        
-
-
     };
 
     const handleChange = (e) => {
@@ -24,7 +38,7 @@ const UserComments = () => {
             <div className="user-comments-img">
                 <FontAwesomeIcon style={{ color: 'grey' }} icon={faUser}></FontAwesomeIcon>
             </div>
-            <textarea value={commentText} onChange={handleChange} name="userComment" id="userComment" className="user-comments-area"></textarea>
+            <textarea value={commentText} onChange={handleChange} name="textarea" id="textarea" className="user-comments-area"></textarea>
             <input className='comment-button' type="submit" value="Submit" onClick={handleSubmit}></input>
         </>
 
