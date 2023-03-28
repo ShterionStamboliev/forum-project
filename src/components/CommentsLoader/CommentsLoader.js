b614d55c273f7c3737d5d56b799c284982822ac7
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2023-03-28 16:10:31 +0300

    Added comments view option for users.

--- src/components/CommentsLoader/CommentsLoader.js
+++ src/components/CommentsLoader/CommentsLoader.js
@@ -1,20 +1,44 @@
 import { faUser } from '@fortawesome/fontawesome-free-regular';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { collection, doc, getDocs, onSnapshot } from 'firebase/firestore';
+import { collection, getDocs, query } from 'firebase/firestore';
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 import { db } from '../../config/firebase';
-
 
 const CommentsLoader = () => {
     const { id } = useParams();
     const [usersComments, setUsersComments] = useState([]);
 
+    const col = query(collection(db, `threads/${id}/comments`));
+
+    const getSnap = async () => {
+        const qSnap = await getDocs(col);
+        let arr = [];
+        let data = [];
+        qSnap.forEach((doc) => {
+            arr.push({ ...doc.data()['comments']});
+        });
+        for (const key of arr.values()) {
+            let a = Object.values(key);
+            for (const v of a) {
+                data.push(v);
+            };
+        };
+        setUsersComments(data);
+    };
+
+    useEffect(() => {
+        const a = new AbortController();
+        getSnap();
+
+        return () => a.abort();
+    }, []);
+
 
     return (
-        <React.Fragment>
-            {Object.values(usersComments).map((x) => {
-                return <>
+        <>
+            {usersComments.map((x) => {
+                return <React.Fragment key={x.id}>
                     <div className="user-comment-icon">
                         <FontAwesomeIcon style={{ color: 'grey' }} icon={faUser}></FontAwesomeIcon>
                     </div>
@@ -26,9 +50,9 @@ const CommentsLoader = () => {
                     <div className="user-comment-name">
                         {x.author}
                     </div>
-                </>
+                </React.Fragment>
             })}
-        </React.Fragment>
+        </>
     );
 };
 
