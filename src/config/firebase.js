6cca5cc1968b6c15388bf15c370a8f9e82947df0
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2023-04-09 09:18:54 +0300

    .

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth"
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDHMEReu674r5KPRaoxWluQj2cbilbz5p4",
-    authDomain: "forum-development-95343.firebaseapp.com",
-    projectId: "forum-development-95343",
-    storageBucket: "forum-development-95343.appspot.com",
-    messagingSenderId: "308657878744",
-    appId: "1:308657878744:web:c1473abbc9ee6308c1c9ad"
+    apiKey: `${process.env.REACT_FIREBASE_API_KEY}`,
+    authDomain: `${process.env.REACT_FIREBASE_AUTH_DOMAIN}`,
+    projectId: `${process.env.REACT_FIREBASE_PROJECT_ID}`,
+    storageBucket: `${process.env.REACT_FIREBASE_STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.REACT_FIREBASE_MESSAGING_SENDER_ID}`,
+    appId: `${process.env.REACT_FIREBASE_MESSAGING_APP_ID}`
 };
 
 const app = initializeApp(firebaseConfig);
