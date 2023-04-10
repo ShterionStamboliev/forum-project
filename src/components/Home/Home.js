13ac41c4b0d8f6d7dd368b6fd0b652fa062288ef
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2023-04-10 21:08:46 +0300

    .

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
 
             <div className="home-welcome">
                 <h1 className="home-info">D4jsp is an internet forum for discussions about Diablo games. <br />
-                    Here, people can ask their questions and create social connections. </h1>
+                    Here, people can ask their questions and create social connections.</h1>
                 <br />
                 <div className="forum-rules">
                     <p>Before continuing, please read the  <Link to='/tos' className='tos-link'>Terms of service </Link></p>
